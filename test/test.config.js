bfbe94be31a146b965f90223d1cdef2d4b92b5f8
Matthew Collier <mcollier@digitalbazaar.com>
2020-10-08 08:27:09 -0400

    Use bedrock-mongodb@8.

--- test/test.config.js
+++ test/test.config.js
@@ -12,7 +12,6 @@ config.mocha.tests.push(path.join(__dirname, 'mocha'));
 config.mongodb.name = 'bedrock_tokenizer_test';
 config.mongodb.host = 'localhost';
 config.mongodb.port = 27017;
-config.mongodb.local.collection = 'bedrock_tokenizer_test';
 // drop all collections on initialization
 config.mongodb.dropCollections = {};
 config.mongodb.dropCollections.onInit = true;

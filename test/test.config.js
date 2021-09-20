f25704b4fe7dc597a00d383efed1a3f945bc3eeb
Ganesh Annan <gannan@digitalbazaar.com>
2021-09-20 15:35:40 -0400

    Simplify test setup with updated `bedrock-meter`.

--- test/test.config.js
+++ test/test.config.js
@@ -20,5 +20,4 @@ config.mongodb.dropCollections.collections = [];
 // HTTPS Agent
 config['https-agent'].rejectUnauthorized = false;
 
-config.tokenizer.kms.baseUrl = `${config.server.baseUri}/kms`;
 config.tokenizer.kms.ipAllowList = ['127.0.0.1/32'];
